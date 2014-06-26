90b80162b08ba48a4f0a1601e45bae4328b1121f
michaelmosher <mmosher47@gmail.com>
2014-06-26 11:29:23 -0500

    various bug fixes

--- overlay.js
+++ overlay.js
@@ -1,15 +1,16 @@
 function resizePopup() {
 	res1 = document.getElementById('resume');
-	res2 = document.getElementById('googleResume');
-	res1.width = res2.width = 0.8 * window.innerWidth;
-	res1.height = res2.height = 0.9 * window.innerHeight;
+	res1.width = 0.8 * window.innerWidth;
+	res1.height = 0.9 * window.innerHeight;
 }
 
 function openOverlay() {
 	resizePopup();
 	document.getElementById('overlay').style.visibility = 'visible';
+	document.body.className = 'noscroll';
 }
 
 function closeOverlay() {
 	document.getElementById('overlay').style.visibility = 'hidden';
+	document.body.className = '';
 }
