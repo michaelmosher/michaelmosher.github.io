1a976b519cb1a7570123c5dfc3e519f647257ec5
michaelmosher <mmosher47@gmail.com>
2014-06-27 15:20:59 -0500

    fixed overlay not 100%

--- overlay.js
+++ overlay.js
@@ -1,15 +1,16 @@
 function resizePopup() {
-	document.getElementById('resume').width = 0.8 * window.innerWidth;
-	document.getElementById('resume').height = 0.9 * window.innerHeight;
+	var viewportWidth = window.innerWidth;
+	var viewportHeight = window.innerHeight;
+	var pageWidth = document.body.clientWidth;
+	document.getElementById('resume').width = 0.8 * Math.min(viewportWidth,pageWidth);
+	document.getElementById('resume').height = Math.min(viewportHeight-100,1.1*pageWidth);
 }
 
 function openOverlay() {
 	resizePopup();
-	document.getElementById('overlay').style.visibility = 'visible';
-	document.body.className = 'noscroll';
+	document.getElementById('overlay').style.display = 'block';
 }
 
 function closeOverlay() {
-	document.getElementById('overlay').style.visibility = 'hidden';
-	document.body.className = '';
+	document.getElementById('overlay').style.display = 'none';
 }
