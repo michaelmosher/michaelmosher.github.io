90495edbf44439104d709c752fe5fffce3373df8
michaelmosher <mmosher47@gmail.com>
2017-08-13 16:27:57 -0400

    axed resume overlay. added toggle-able info to footer

--- public/js/app.bundle.js
+++ public/js/app.bundle.js
@@ -54420,50 +54420,117 @@ exports.default = Header;
 
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+	value: true
 });
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = __webpack_require__(4);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactRouterDom = __webpack_require__(29);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function openOverlay() {
-    resizePopup();
-    document.getElementById('overlay').style.display = 'block';
-}
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Footer = function Footer() {
-    return _react2.default.createElement(
-        'footer',
-        null,
-        _react2.default.createElement(
-            'p',
-            null,
-            'Still want to know more? Check out ',
-            _react2.default.createElement(
-                'a',
-                { id: 'overlayLink', href: '#', onClick: openOverlay },
-                'my resum\xE9'
-            ),
-            '.'
-        ),
-        _react2.default.createElement(
-            'p',
-            null,
-            'Want to talk? Email me at ',
-            _react2.default.createElement(
-                'a',
-                { href: 'mailto:mmosher47@gmail.com', target: '_top' },
-                'mmosher47@gmail.com'
-            ),
-            '.'
-        )
-    );
-};
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Footer = function (_React$Component) {
+	_inherits(Footer, _React$Component);
+
+	function Footer() {
+		_classCallCheck(this, Footer);
+
+		var _this = _possibleConstructorReturn(this, (Footer.__proto__ || Object.getPrototypeOf(Footer)).call(this));
+
+		_this.state = { expanded: false };
+
+		_this.toggleInfo = _this.toggleInfo.bind(_this);
+
+		return _this;
+	}
+
+	_createClass(Footer, [{
+		key: 'toggleInfo',
+		value: function toggleInfo() {
+			this.setState(function (prevState) {
+				return { expanded: !prevState.expanded };
+			});
+		}
+	}, {
+		key: 'render',
+		value: function render() {
+			var info = _react2.default.createElement(
+				'a',
+				{ onClick: this.toggleInfo },
+				'Want to know more?'
+			);
+
+			if (this.state.expanded) {
+				var expandedInfoStyle = {
+					maxWidth: '800px',
+					backgroundColor: '#000050',
+					border: 'solid #000030 4px',
+					margin: '0 auto',
+					padding: '5px'
+				};
+				info = _react2.default.createElement(
+					'div',
+					{ style: expandedInfoStyle },
+					_react2.default.createElement(
+						'p',
+						null,
+						'My resum\xE9 is hosted on ',
+						_react2.default.createElement(
+							'a',
+							{ href: 'https://github.com/michaelmosher/LatexResume' },
+							'GitHub'
+						),
+						' as well.'
+					),
+					_react2.default.createElement(
+						'p',
+						null,
+						'Want to talk? Email me at ',
+						_react2.default.createElement(
+							'a',
+							{ href: 'mailto:mmosher47@gmail.com', target: '_top' },
+							'mmosher47@gmail.com'
+						),
+						'.'
+					),
+					_react2.default.createElement(
+						'a',
+						{ onClick: this.toggleInfo },
+						'Want to know less?'
+					)
+				);
+			}
+
+			return _react2.default.createElement(
+				'footer',
+				null,
+				_react2.default.createElement(
+					'p',
+					null,
+					'This webpage is hosted on ',
+					_react2.default.createElement(
+						'a',
+						{ href: 'https://github.com/michaelmosher/michaelmosher.github.io' },
+						'GitHub'
+					),
+					'.'
+				),
+				info
+			);
+		}
+	}]);
+
+	return Footer;
+}(_react2.default.Component);
+
 exports.default = Footer;
 
 /***/ })
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-function openOverlay() {
-	resizePopup();
-	document.getElementById('overlay').style.display = 'block';
+export default class Footer extends React.Component {
+    constructor() {
+        super()
+		this.state = { expanded: false}
+
+		this.toggleInfo = this.toggleInfo.bind(this);
+
+	}
+
+	toggleInfo() {
+		this.setState(prevState => {
+			return {expanded: !prevState.expanded}
+		})
+	}
+
+	render() {
+		let info = <a onClick={this.toggleInfo}>Want to know more?</a>
+
+		if (this.state.expanded) {
+			const expandedInfoStyle = {
+				maxWidth: '800px',
+				backgroundColor: '#000050',
+				border: 'solid #000030 4px',
+				margin: '0 auto',
+				padding: '5px'
+			}
+			info = <div style={expandedInfoStyle}>
+				<p>My resum&#233; is hosted on <a href='https://github.com/michaelmosher/LatexResume'>GitHub</a> as well.</p>
+				<p>Want to talk? Email me at <a href='mailto:mmosher47@gmail.com' target='_top'>mmosher47@gmail.com</a>.</p>
+				<a onClick={this.toggleInfo}>Want to know less?</a>
+			</div>
+		}
+
+		return (
+			<footer>
+				<p>This webpage is hosted on <a href='https://github.com/michaelmosher/michaelmosher.github.io'>GitHub</a>.</p>
+				{info}
+			</footer>
+		)
+	}
 }
-
-const Footer = () => (
-    <footer>
-        <p>Still want to know more? Check out <a id='overlayLink' href='#' onClick={openOverlay} >my resum&#233;</a>.</p>
-		<p>Want to talk? Email me at <a href='mailto:mmosher47@gmail.com' target='_top'>mmosher47@gmail.com</a>.</p>
-    </footer>
-)
-export default Footer
